f508ebd9762ac1b45466a28a09618cac7a091d02
lucasmv2205 <lmartins2205@gmail.com>
2022-06-26 17:58:01 -0300

    start project with initials styles and necessary dependencies

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { GlobalStyle } from "./styles/global";
+
+function App() {
+
+  return (
+    <>
+      <GlobalStyle />
+      <h3>Home</h3>
+    </>
+  )
+}
+
+export default App
---
+++ src/styles/global.ts
@@ -0,0 +1,44 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  :root{
+      --footer-background: #FAFAFA;
+      --border: #DCDCDC;
+      --yellow: #FAD34F;
+  }
+
+  *{
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box
+  }
+
+  html{
+      @media(max-width: 1080px){
+          font-size: 93.75%; // 15px
+      }
+
+      @media(max-width: 720px){
+          font-size: 87.5%; // 14px
+      }
+  }
+
+  body{
+      background: var(--background);
+      -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button{
+      font-family: 'Apercu Arabic Pro', sans-serif;
+      font-weight: 400;
+  }
+
+  h1,h2,h3,h4,h5,h6,strong{
+      font-weight: 600;
+  }
+
+  button{
+      cursor: pointer;
+  }
+
+`;
