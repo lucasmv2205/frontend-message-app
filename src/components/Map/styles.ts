c8abc10644e043e3376e4c8fae3c6ce39ab04bcb
lucasmv2205 <lmartins2205@gmail.com>
2022-06-28 00:33:16 -0300

    add image map, the ideal is to use the react map library

---
+++ src/components/Map/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Image = styled.img`
+  border-radius: 50%;
+  height: 50rem;
+  width: 50rem;
+  object-fit: fill;
+  position: absolute;
+  right: -10%;
+`
--- src/page/styles.ts
+++ src/page/styles.ts
@@ -7,8 +7,8 @@ export const Container = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
-  justify-content: space-around;
-  /* margin-left: 5rem; */
+  justify-content: start;
+  margin-left: 5rem;
 `;
 
 export const ImageTopYellow = styled.img`
