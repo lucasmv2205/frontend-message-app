0e4d6e2ba3916f285d5bb2da51a9b7a9834d2ace
lucasmv2205 <lmartins2205@gmail.com>
2022-06-28 00:43:13 -0300

    Revert "add react map library"
    This reverts commit e00f4fa7918644b2424470b907fa9ca23c0c09d2.

--- src/page/styles.ts
+++ src/page/styles.ts
@@ -7,9 +7,8 @@ export const Container = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
-  justify-content: space-around;
-  /* justify-content: start; */
-  /* margin-left: 5rem; */
+  justify-content: start;
+  margin-left: 5rem;
 `;
 
 export const ImageTopYellow = styled.img`
